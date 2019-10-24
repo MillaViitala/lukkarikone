a39c6702aaa790a31abd9aa82a8cdd7f09ddeaa3
Milla Viitala <bgg339@myy.haaga-helia.fi>
2019-10-24 12:41:32 +0300

    Eka

---
+++ lukkarikone/src/main/java/com/example/lukkarikone/LukkarikoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.lukkarikone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LukkarikoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LukkarikoneApplication.class, args);
+	}
+
+}
---
+++ lukkarikone/src/test/java/com/example/lukkarikone/LukkarikoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lukkarikone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LukkarikoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
