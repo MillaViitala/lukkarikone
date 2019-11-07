0df6f67da7d121e0b48348fbe5b3c4d308dd27e1
Milla Viitala <bgg339@myy.haaga-helia.fi>
2019-11-07 12:17:16 +0200

    CrossOrigin lisätty

--- lukkarikone/src/main/java/com/example/lukkarikone/web/SurveyController.java
+++ lukkarikone/src/main/java/com/example/lukkarikone/web/SurveyController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.example.lukkarikone.domain.Question;
 import com.example.lukkarikone.domain.QuestionRepository;
 
-
+@CrossOrigin
 @Controller
 public class SurveyController {
 
