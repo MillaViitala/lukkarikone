a3c243d3a63dc3ade81bbfdcbd1736d385861b08
niklas <niklas@localhost.localdomain>
2019-11-12 14:01:31 +0200

    Fixed rest endpoints

--- lukkarikone/src/main/java/com/example/lukkarikone/web/SurveyController.java
+++ lukkarikone/src/main/java/com/example/lukkarikone/web/SurveyController.java
@@ -5,10 +5,15 @@ import com.example.lukkarikone.domain.QuestionRepository;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,39 +31,11 @@ public class SurveyController {
         return "homepage";
     }
 
-    // Näytä kaikki kysymykset
-    @RequestMapping(value = "/questionlist")
-    public String questionList(Model model) {
-        model.addAttribute("questions", qrepository.findAll());
-        return "questionlist";
-    }
-
     // REST kysymyksille
     @RequestMapping(value = "/questions", method = RequestMethod.GET)
     public @ResponseBody
     List<Question> questionListRest() {
         return (List<Question>) qrepository.findAll();
-    }
-
-    // Lisää kysymys
-    @RequestMapping(value = "/add")
-    public String addQuestion(Model model) {
-        model.addAttribute("question", new Question());
-        return "addquestion";
-    }
-
-    // Tallenna kysymys
-    @RequestMapping(value = "/save", method = RequestMethod.POST)
-    public String save(Question question) {
-        qrepository.save(question);
-        return "redirect:questionlist";
-    }
-
-    // Poista kysymys
-    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
-    public String deleteQuestion(@PathVariable("id") Long questionId, Model model) {
-        qrepository.deleteById(questionId);
-        return "redirect:../questionlist";
     }
 
     // Hae kysymys
@@ -68,19 +45,17 @@ public class SurveyController {
         return qrepository.findById(questionId);
     }
 
-//    FIX THESE
-//    //update: tallenna kysymys
-//    @RequestMapping(value = "/questions", method = RequestMethod.POST)
-//    public String save(@RequestBody String question, Model model){
-//        Question q = new Question(question);
-//        qrepository.save(q);
-//        return "redirect:../homepage";
-//    }
-//    
-//    // Poista kysymys
-//    @RequestMapping(value = "/questions/{id}", method = RequestMethod.DELETE)
-//    public String deleteQuestion(@PathVariable("id") Long questionId, Model model) {
-//    	qrepository.deleteById(questionId);
-//        return "redirect:../homepage";
-//    }
+    //update: tallenna kysymys
+    @PostMapping(value = "/questions", consumes = "application/json")
+    public ResponseEntity saveQuestion(@RequestBody Question question, Model model) {
+        qrepository.save(new Question(question.getQtitle()));
+        return new ResponseEntity<>("Question created!", HttpStatus.OK);
+    }
+
+    // Poista kysymys
+    @DeleteMapping(value = "/questions/{id}")
+    public ResponseEntity deleteQuestion(@PathVariable("id") Long questionId, Model model) {
+        qrepository.deleteById(questionId);
+        return new ResponseEntity<>("Question with id: " + questionId + " deleted!", HttpStatus.OK);
+    }
 }
