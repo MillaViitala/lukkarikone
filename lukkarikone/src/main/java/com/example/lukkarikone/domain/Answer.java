3383d6759749e7fb52cbec2e42bac4ac1dad121f
Milla Viitala <bgg339@myy.haaga-helia.fi>
2019-11-14 11:57:56 +0200

    Vastausluokat muokattu

--- lukkarikone/src/main/java/com/example/lukkarikone/domain/Answer.java
+++ lukkarikone/src/main/java/com/example/lukkarikone/domain/Answer.java
@@ -1,10 +1,28 @@
 package com.example.lukkarikone.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Answer {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String text;
 	private int QuestionId;
+	
+	public Answer() {
+		
+	}
+	
+	public Answer(String text) {
+		super();
+		this.text = text;
+		
+	}
 
 	public Long getId() {
 		return id;
--- lukkarikone/src/main/java/com/example/lukkarikone/web/AnswerController.java
+++ lukkarikone/src/main/java/com/example/lukkarikone/web/AnswerController.java
@@ -1,8 +1,9 @@
 package com.example.lukkarikone.web;
 
 import com.example.lukkarikone.domain.Answer;
+
 import com.example.lukkarikone.domain.AnswerRepository;
-import java.util.List;
+
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,7 +27,7 @@ public class AnswerController {
 	@Autowired
 	private AnswerRepository arepository; 
 	
-	// Hae vastaukset kysymykselle. En tiiä toimiiko
+	// Hae vastaukset kysymykselle
 	
 	@RequestMapping(value = "questions/{questionId}/answers", method = RequestMethod.GET)
 	public @ResponseBody Optional<Answer> getAnswer(@PathVariable ("id") Long answerId, Model model) {
@@ -35,6 +35,14 @@ public class AnswerController {
 		return arepository.findById(answerId);
 		
 	}
+	
+	// Tallenna vastaus
+	
+	@PostMapping(value = "/questions/{questionId}/answers", consumes = "application/json")
+    public ResponseEntity saveAnswer(@RequestBody Answer answer, Model model) {
+        arepository.save(new Answer(answer.getText()));
+        return new ResponseEntity<>("Answer saved!", HttpStatus.OK);
+    }
 	
 	
 }
