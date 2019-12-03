b45cd19f8c9a373f8aa51670e59927f114a1dfcb
ElinaKatariina <ElinaKatariina@users.noreply.github.com>
2019-12-03 14:14:31 +0200

    ManytoOne

--- lukkarikone/src/main/java/com/example/lukkarikone/domain/Question.java
+++ lukkarikone/src/main/java/com/example/lukkarikone/domain/Question.java
@@ -5,6 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
@@ -16,7 +17,7 @@ public class Question {
     private Long id;
     private String qtitle;
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "categoryId")
     private QuestionCategory category;
 
